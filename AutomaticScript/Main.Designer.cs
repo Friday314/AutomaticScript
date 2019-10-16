66c63b48da71eccc42226035402ae905ea29a3d9
张正 <zhangzheng@email.com>
2019-10-16 17:41:12 +0800

    Commit

--- AutomaticScript/Main.Designer.cs
+++ AutomaticScript/Main.Designer.cs
@@ -63,9 +63,9 @@
             this.ClientSize = new System.Drawing.Size(666, 352);
             this.Controls.Add(this.日志输出);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "AutomaticScript";
             this.Text = "AutomaticScript";
             this.Load += new System.EventHandler(this.AutomaticScript_Load);
--- AutomaticScript/Main.cs
+++ AutomaticScript/Main.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +13,12 @@ namespace AutomaticScript
 {
     public partial class AutomaticScript : Form
     {
+        DmReg dd = new DmReg();
+
         public AutomaticScript()
         {
             InitializeComponent();
         }
-
         private void AutomaticScript_Load(object sender, EventArgs e)
         {
             textLog.ReadOnly = true;
